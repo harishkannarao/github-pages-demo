8a08dbbacc437501234965ccea6c4461aff363b7
harishkannarao <harishkannarao@users.noreply.github.com>
2023-07-07 23:53:11 +0100

    Display with bigger font

--- app/time/page.tsx
+++ app/time/page.tsx
@@ -2,7 +2,7 @@ import { CurrentTime } from "../../components/time/time";
 
 export default function Page() {
     return (
-        <h1 className="badge bg-primary">
+        <h1 className="bg-primary">
             <span>Current UTC time: </span><CurrentTime />
         </h1>
     )
