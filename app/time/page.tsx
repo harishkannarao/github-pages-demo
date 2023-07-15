896ca4af366e4a9ccf6f6fa2fd2d14b6dea47839
harishkannarao <harishkannarao@users.noreply.github.com>
2023-07-15 15:51:20 +0100

    Amending time display message

--- app/time/page.tsx
+++ app/time/page.tsx
@@ -5,7 +5,7 @@ import { CurrentTime } from "../../components/time/time";
 export default function Page() {
     return (
         <h1 className="bg-primary">
-            <span>Current UTC time: </span><CurrentTime />
+            <span>Current UTC time is: </span><CurrentTime />
         </h1>
     )
 }
