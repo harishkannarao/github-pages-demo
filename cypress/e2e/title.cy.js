6c71a8a84dd380554ba333e27a41bc7e1e3be346
harishkannarao <harishkannarao@users.noreply.github.com>
2023-09-18 17:20:24 +0100

    Adding cypress test for title

---
+++ cypress/e2e/title.cy.js
@@ -0,0 +1,26 @@
+describe('Display Page Title', () => {
+    it('should display title correctly', () => {
+        cy.visit(Cypress.env('host'))
+
+        cy.get('a[data-testid="dynamic-title-link"]')
+            .should('have.length', 1)
+            .click()
+
+        cy.title().should('eq', 'My Sample Site')
+
+        cy.get('[data-testid="new-title-context"]')
+            .should('have.length', 1)
+            .click()
+            .clear()
+            .type('context title')
+
+        cy.title().should('eq', 'context title')
+
+        cy.get('[data-testid="new-title-context"]')
+            .should('have.length', 1)
+            .click()
+            .clear()
+
+        cy.title().should('eq', 'My Sample Site')
+    })
+})
