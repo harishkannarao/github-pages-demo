1f9bbe6529a63b72dd366aee05ec5c082a60b554
harishkannarao <harishkannarao@users.noreply.github.com>
2023-09-23 09:45:35 +0100

    Adding ability to change base path

--- next.config.js
+++ next.config.js
@@ -5,11 +5,12 @@ const path = require('path')
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  basePath: process.env.NEXT_BASE_PATH || '',
   output: 'export',
   // Optional: Add a trailing slash to all paths `/about` -> `/about/`
   trailingSlash: true,
   // Optional: Change the output directory `out` -> `dist`
-  distDir: 'out',
+  distDir: 'out' + process.env.NEXT_BASE_PATH || '',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
